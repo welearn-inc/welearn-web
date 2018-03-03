d53a7fe46d4239b9ef7d28643b0f914fd9271761
Sagar Manchadi <sagar.manchadi@quicksoftpro.com>
2018-03-03 18:00:25 +0530

    header created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@app/material.module';
 import { LoaderComponent } from './loader/loader.component';
+import { HeaderComponent } from './header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
+
+
 
 @NgModule({
   imports: [
     FlexLayoutModule,
     MaterialModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    TranslateModule
   ],
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    HeaderComponent
   ],
   exports: [
-    LoaderComponent
+    LoaderComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
